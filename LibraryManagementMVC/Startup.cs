441baf7c884b49a93a8810212a75d14e19cc4b06
Rhys Grabany <r.grabany@gmail.com>
2020-12-06 18:30:52 +0000

    Removing useless Razor stuff, using partial views

--- LibraryManagementMVC/Startup.cs
+++ LibraryManagementMVC/Startup.cs
@@ -31,8 +31,6 @@ namespace LibraryManagementMVC
             });
             services.AddControllersWithViews();
             services.AddScoped<IDataConnection,SqlConnector>();
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,8 +58,6 @@ namespace LibraryManagementMVC
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-                endpoints.MapBlazorHub();
             });
         }
     }
